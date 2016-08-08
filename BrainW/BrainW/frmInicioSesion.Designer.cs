a04e1cc5625ca168b909b813d9a537fa3c4aa36d
Roberto González Cruz <the-king_bone@hotmail.com>
2016-08-08 07:50:07 -0700

    terminado login

--- BrainW/BrainW/frmInicioSesion.Designer.cs
+++ BrainW/BrainW/frmInicioSesion.Designer.cs
@@ -31,20 +31,20 @@
             this.txtB_Usuario = new System.Windows.Forms.TextBox();
             this.txtB_Contraseña = new System.Windows.Forms.TextBox();
             this.bttn_Aceptar = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_Usuario = new System.Windows.Forms.Label();
+            this.lbl_Contraseña = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtB_Usuario
             // 
-            this.txtB_Usuario.Location = new System.Drawing.Point(92, 82);
+            this.txtB_Usuario.Location = new System.Drawing.Point(92, 87);
             this.txtB_Usuario.Name = "txtB_Usuario";
             this.txtB_Usuario.Size = new System.Drawing.Size(100, 20);
             this.txtB_Usuario.TabIndex = 0;
             // 
             // txtB_Contraseña
             // 
-            this.txtB_Contraseña.Location = new System.Drawing.Point(92, 125);
+            this.txtB_Contraseña.Location = new System.Drawing.Point(92, 134);
             this.txtB_Contraseña.Name = "txtB_Contraseña";
             this.txtB_Contraseña.PasswordChar = '*';
             this.txtB_Contraseña.Size = new System.Drawing.Size(100, 20);
@@ -52,38 +52,39 @@
             // 
             // bttn_Aceptar
             // 
-            this.bttn_Aceptar.Location = new System.Drawing.Point(105, 160);
+            this.bttn_Aceptar.Location = new System.Drawing.Point(105, 169);
             this.bttn_Aceptar.Name = "bttn_Aceptar";
             this.bttn_Aceptar.Size = new System.Drawing.Size(75, 23);
             this.bttn_Aceptar.TabIndex = 2;
             this.bttn_Aceptar.Text = "Aceptar";
             this.bttn_Aceptar.UseVisualStyleBackColor = true;
+            this.bttn_Aceptar.Click += new System.EventHandler(this.bttn_Aceptar_Click);
             // 
-            // label1
+            // lbl_Usuario
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(121, 63);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Usuario";
+            this.lbl_Usuario.AutoSize = true;
+            this.lbl_Usuario.Location = new System.Drawing.Point(121, 68);
+            this.lbl_Usuario.Name = "lbl_Usuario";
+            this.lbl_Usuario.Size = new System.Drawing.Size(43, 13);
+            this.lbl_Usuario.TabIndex = 3;
+            this.lbl_Usuario.Text = "Usuario";
             // 
-            // label2
+            // lbl_Contraseña
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(112, 109);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Contraseña";
+            this.lbl_Contraseña.AutoSize = true;
+            this.lbl_Contraseña.Location = new System.Drawing.Point(112, 118);
+            this.lbl_Contraseña.Name = "lbl_Contraseña";
+            this.lbl_Contraseña.Size = new System.Drawing.Size(61, 13);
+            this.lbl_Contraseña.TabIndex = 4;
+            this.lbl_Contraseña.Text = "Contraseña";
             // 
             // FRM_InicioSesion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_Contraseña);
+            this.Controls.Add(this.lbl_Usuario);
             this.Controls.Add(this.bttn_Aceptar);
             this.Controls.Add(this.txtB_Usuario);
             this.Controls.Add(this.txtB_Contraseña);
@@ -100,8 +101,8 @@
         private System.Windows.Forms.TextBox txtB_Usuario;
         private System.Windows.Forms.TextBox txtB_Contraseña;
         private System.Windows.Forms.Button bttn_Aceptar;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_Usuario;
+        private System.Windows.Forms.Label lbl_Contraseña;
     }
 }
 
---
+++ BrainW/BrainW/frmInicioSesion.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace BrainW
+{
+    public partial class FRM_InicioSesion : Form
+    {
+        public string stringconnection = "Database=pfpa92; DataSource=localhost; Uid=root; pwd=1234";
+        public MySqlCommand cmd = new MySqlCommand();
+        public MySqlDataAdapter adapter = new MySqlDataAdapter();
+        public DataSet dataset = new DataSet();
+
+        public FRM_InicioSesion()
+        {
+            InitializeComponent();
+        }
+
+        private void login()
+        {
+            try
+            {
+                MySqlConnection conn = new MySqlConnection(stringconnection);
+                cmd.CommandText = "SELECT id_u, user, password, type FROM tblusuarios";
+                cmd.Connection = conn;
+                if (conn.State == 0)
+                    conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                adapter.SelectCommand = cmd;
+                adapter.Fill(dataset, "tblusuarios");
+                if (dataset.Tables[0].Rows.Count > 0)
+                {
+                    for (int rowC = 0; rowC < dataset.Tables[0].Rows.Count; rowC++)
+                    {
+                        if (txtB_Usuario.Text == dataset.Tables[0].Rows[rowC].ItemArray[1].ToString() && txtB_Contraseña.Text == dataset.Tables[0].Rows[rowC].ItemArray[2].ToString())
+                        {
+                            this.Visible = false;
+                            frmMenu menu = new frmMenu();
+                            menu.Show();
+                            break;
+                        }
+                        else
+                        {
+                            if (rowC == dataset.Tables[0].Rows.Count - 1)
+                            {
+                                MessageBox.Show("Usuario/Contraseña incorrectos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                txtB_Usuario.Clear();
+                                txtB_Contraseña.Clear();
+                                txtB_Usuario.Focus();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception excp)
+            {
+                MessageBox.Show(excp.Message);
+            }
+        }
+
+
+        private void FRM_InicioSesion_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bttn_Aceptar_Click(object sender, EventArgs e)
+        {
+            login();
+        }
+    }
+}
