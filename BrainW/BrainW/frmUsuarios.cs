c6be3b8054b5eaa2d703edeeab7c70d15af3b8d4
Roberto González Cruz <the-king_bone@hotmail.com>
2016-08-10 11:16:37 -0700

    Agregar usuarios completo

--- BrainW/BrainW/frmUsuarios.cs
+++ BrainW/BrainW/frmUsuarios.cs
@@ -7,14 +7,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Odbc;
+using MySql.Data.MySqlClient;
 
 namespace BrainW
 {
     public partial class frmUsuarios : Form
     {
+        dbConn db = new dbConn();
+
         public frmUsuarios()
         {
             InitializeComponent();
+        }
+
+        public int getTipo(string _tipo)
+        {
+            int tipo = 0;
+            if (_tipo == "Usuario")
+                tipo = 1;
+            return tipo;
+        }
+
+         
+
+        private void AgregarUsuario()
+        {
+            db.cmd.CommandText = ("INSERT INTO tblusuarios VALUES (user, password, nivel) VALUES ('" + txtUsuario.Text + "','" + txtContrasena.Text + "','" + getTipo(comboBoxTipoCuenta.SelectedItem.ToString()) + "')");
+            db.cmd.Connection = db.conn;
+            if (db.conn.State == 0)
+                db.conn.Open();
+            db.cmd.ExecuteNonQuery();
+            db.conn.Close();
+            MessageBox.Show("Usuario registrado con exito");
+        }
+
+        private void EditarUsurio()
+        {
+            //db.cmd.CommandText = ("UPDATE tblusuario SET user = '" + txtUsuario.Text + "', password = '" + txtContrasena.Text + "', nivel = '" + getTipo(comboBoxTipoCuenta.SelectedItem.ToString() + "')");
+        }
+
+        private void cargarDatos()
+        {
+            db.conn.Open();
+            //OdbcDataAdapter da = new OdbcDataAdapter();
+            MySqlDataAdapter da = new MySqlDataAdapter("SELECT * FROM tblusuarios order by id_u", db.conn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            db.conn.Close();
+            dt.Dispose();
+        }
+
+        private void frmUsuarios_Load(object sender, EventArgs e)
+        {
+            cargarDatos();
         }
     }
 }
