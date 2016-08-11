c243364872af822a22356475f7258fe279427d37
Roberto González Cruz <the-king_bone@hotmail.com>
2016-08-10 17:40:02 -0700

    Avances

--- BrainW/BrainW/Properties/Settings.Designer.cs
+++ BrainW/BrainW/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace BrainW.Properties
-{
-
-
+namespace BrainW.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("User Id=root;Password=1234;Host=localhost;Database=pfpa92;SSH Authentication Type" +
+            "=Password")]
+        public string pfpa92ConnectionString {
+            get {
+                return ((string)(this["pfpa92ConnectionString"]));
             }
         }
     }
--- BrainW/BrainW/frmUsuarios.Designer.cs
+++ BrainW/BrainW/frmUsuarios.Designer.cs
@@ -28,7 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmUsuarios));
+            System.Windows.Forms.Label lblID;
             this.txtUsuario = new System.Windows.Forms.TextBox();
             this.txtContrasena = new System.Windows.Forms.TextBox();
             this.txtVerifContra = new System.Windows.Forms.TextBox();
@@ -45,10 +47,17 @@
             this.toolStripButtonEliminar = new System.Windows.Forms.ToolStripButton();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pfpa92DataSet = new BrainW.pfpa92DataSet();
+            this.tblusuariosBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tblusuariosTableAdapter = new BrainW.pfpa92DataSetTableAdapters.tblusuariosTableAdapter();
+            this.txtID = new System.Windows.Forms.TextBox();
+            lblID = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.toolStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pfpa92DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblusuariosBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // txtUsuario
@@ -62,15 +71,19 @@
             // 
             this.txtContrasena.Location = new System.Drawing.Point(196, 63);
             this.txtContrasena.Name = "txtContrasena";
+            this.txtContrasena.PasswordChar = '*';
             this.txtContrasena.Size = new System.Drawing.Size(121, 20);
             this.txtContrasena.TabIndex = 1;
+            this.txtContrasena.TextChanged += new System.EventHandler(this.txtContrasena_TextChanged);
             // 
             // txtVerifContra
             // 
             this.txtVerifContra.Location = new System.Drawing.Point(196, 89);
             this.txtVerifContra.Name = "txtVerifContra";
+            this.txtVerifContra.PasswordChar = '*';
             this.txtVerifContra.Size = new System.Drawing.Size(121, 20);
             this.txtVerifContra.TabIndex = 2;
+            this.txtVerifContra.TextChanged += new System.EventHandler(this.txtVerifContra_TextChanged);
             // 
             // comboBoxTipoCuenta
             // 
@@ -131,6 +144,8 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(lblID);
+            this.groupBox1.Controls.Add(this.txtID);
             this.groupBox1.Controls.Add(this.pictureBox1);
             this.groupBox1.Controls.Add(this.lblUsuario);
             this.groupBox1.Controls.Add(this.txtUsuario);
@@ -168,6 +183,7 @@
             this.toolStripButtonAgregar.Name = "toolStripButtonAgregar";
             this.toolStripButtonAgregar.Size = new System.Drawing.Size(23, 22);
             this.toolStripButtonAgregar.Text = "Agregar";
+            this.toolStripButtonAgregar.CheckStateChanged += new System.EventHandler(this.toolStripButtonAgregar_CheckStateChanged);
             // 
             // toolStripButtonEditar
             // 
@@ -190,29 +206,70 @@
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBox1.Location = new System.Drawing.Point(323, 89);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(21, 20);
+            this.pictureBox1.Size = new System.Drawing.Size(22, 20);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 10;
             this.pictureBox1.TabStop = false;
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
             this.dataGridView1.Location = new System.Drawing.Point(12, 236);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
             this.dataGridView1.Size = new System.Drawing.Size(511, 150);
             this.dataGridView1.TabIndex = 11;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // pfpa92DataSet
+            // 
+            this.pfpa92DataSet.DataSetName = "pfpa92DataSet";
+            this.pfpa92DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tblusuariosBindingSource
+            // 
+            this.tblusuariosBindingSource.DataMember = "tblusuarios";
+            this.tblusuariosBindingSource.DataSource = this.pfpa92DataSet;
+            // 
+            // tblusuariosTableAdapter
+            // 
+            this.tblusuariosTableAdapter.ClearBeforeFill = true;
+            // 
+            // txtID
+            // 
+            this.txtID.Enabled = false;
+            this.txtID.Location = new System.Drawing.Point(196, 11);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(121, 20);
+            this.txtID.TabIndex = 12;
+            // 
+            // lblID
+            // 
+            lblID.AutoSize = true;
+            lblID.Location = new System.Drawing.Point(57, 18);
+            lblID.Name = "lblID";
+            lblID.Size = new System.Drawing.Size(18, 13);
+            lblID.TabIndex = 13;
+            lblID.Text = "ID";
             // 
             // frmUsuarios
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(535, 415);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.groupBox1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmUsuarios";
             this.Text = "Usuarios";
             this.Load += new System.EventHandler(this.frmUsuarios_Load);
@@ -222,6 +279,8 @@
             this.toolStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pfpa92DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblusuariosBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -245,5 +304,9 @@
         private System.Windows.Forms.ToolStripButton toolStripButtonEditar;
         private System.Windows.Forms.ToolStripButton toolStripButtonEliminar;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private pfpa92DataSet pfpa92DataSet;
+        private System.Windows.Forms.BindingSource tblusuariosBindingSource;
+        private pfpa92DataSetTableAdapters.tblusuariosTableAdapter tblusuariosTableAdapter;
+        private System.Windows.Forms.TextBox txtID;
     }
 }
