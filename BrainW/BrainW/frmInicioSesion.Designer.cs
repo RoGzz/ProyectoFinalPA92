f35c5e811705dd48dc9e3d0a73abb66af3bde8d2
Roberto González Cruz <the-king_bone@hotmail.com>
2016-08-12 14:38:40 -0700

    Agregar, Editar, Eliminar usarios completado (faltan validaciones).

--- BrainW/BrainW/frmInicioSesion.Designer.cs
+++ BrainW/BrainW/frmInicioSesion.Designer.cs
@@ -80,6 +80,7 @@
             // 
             // frmInicioSesion
             // 
+            this.AcceptButton = this.bttn_Aceptar;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
--- BrainW/BrainW/frmUsuarios.Designer.cs
+++ BrainW/BrainW/frmUsuarios.Designer.cs
@@ -29,8 +29,8 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmUsuarios));
             System.Windows.Forms.Label lblID;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmUsuarios));
             this.txtUsuario = new System.Windows.Forms.TextBox();
             this.txtContrasena = new System.Windows.Forms.TextBox();
             this.txtVerifContra = new System.Windows.Forms.TextBox();
@@ -41,30 +41,39 @@
             this.lblTipoCuenta = new System.Windows.Forms.Label();
             this.bttnAceptar = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.toolStripButtonAgregar = new System.Windows.Forms.ToolStripButton();
             this.toolStripButtonEditar = new System.Windows.Forms.ToolStripButton();
             this.toolStripButtonEliminar = new System.Windows.Forms.ToolStripButton();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.pfpa92DataSet = new BrainW.pfpa92DataSet();
             this.tblusuariosBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.pfpa92DataSet = new BrainW.pfpa92DataSet();
             this.tblusuariosTableAdapter = new BrainW.pfpa92DataSetTableAdapters.tblusuariosTableAdapter();
-            this.txtID = new System.Windows.Forms.TextBox();
             lblID = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
-            this.toolStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pfpa92DataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.tblusuariosBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pfpa92DataSet)).BeginInit();
             this.SuspendLayout();
+            // 
+            // lblID
+            // 
+            lblID.AutoSize = true;
+            lblID.Location = new System.Drawing.Point(57, 18);
+            lblID.Name = "lblID";
+            lblID.Size = new System.Drawing.Size(18, 13);
+            lblID.TabIndex = 13;
+            lblID.Text = "ID";
             // 
             // txtUsuario
             // 
             this.txtUsuario.Location = new System.Drawing.Point(196, 37);
             this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(121, 20);
+            this.txtUsuario.Size = new System.Drawing.Size(281, 20);
             this.txtUsuario.TabIndex = 0;
             // 
             // txtContrasena
@@ -72,7 +81,7 @@
             this.txtContrasena.Location = new System.Drawing.Point(196, 63);
             this.txtContrasena.Name = "txtContrasena";
             this.txtContrasena.PasswordChar = '*';
-            this.txtContrasena.Size = new System.Drawing.Size(121, 20);
+            this.txtContrasena.Size = new System.Drawing.Size(281, 20);
             this.txtContrasena.TabIndex = 1;
             this.txtContrasena.TextChanged += new System.EventHandler(this.txtContrasena_TextChanged);
             // 
@@ -81,7 +90,7 @@
             this.txtVerifContra.Location = new System.Drawing.Point(196, 89);
             this.txtVerifContra.Name = "txtVerifContra";
             this.txtVerifContra.PasswordChar = '*';
-            this.txtVerifContra.Size = new System.Drawing.Size(121, 20);
+            this.txtVerifContra.Size = new System.Drawing.Size(281, 20);
             this.txtVerifContra.TabIndex = 2;
             this.txtVerifContra.TextChanged += new System.EventHandler(this.txtVerifContra_TextChanged);
             // 
@@ -93,7 +102,7 @@
             "Usuario"});
             this.comboBoxTipoCuenta.Location = new System.Drawing.Point(196, 115);
             this.comboBoxTipoCuenta.Name = "comboBoxTipoCuenta";
-            this.comboBoxTipoCuenta.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxTipoCuenta.Size = new System.Drawing.Size(281, 21);
             this.comboBoxTipoCuenta.TabIndex = 3;
             // 
             // lblUsuario
@@ -134,7 +143,7 @@
             // 
             // bttnAceptar
             // 
-            this.bttnAceptar.Location = new System.Drawing.Point(150, 162);
+            this.bttnAceptar.Location = new System.Drawing.Point(218, 153);
             this.bttnAceptar.Name = "bttnAceptar";
             this.bttnAceptar.Size = new System.Drawing.Size(75, 23);
             this.bttnAceptar.TabIndex = 8;
@@ -156,113 +165,127 @@
             this.groupBox1.Controls.Add(this.txtVerifContra);
             this.groupBox1.Controls.Add(this.lblContrasena);
             this.groupBox1.Controls.Add(this.comboBoxTipoCuenta);
-            this.groupBox1.Location = new System.Drawing.Point(80, 28);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(374, 191);
+            this.groupBox1.Size = new System.Drawing.Size(511, 191);
             this.groupBox1.TabIndex = 9;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Datos";
-            // 
-            // toolStrip1
-            // 
-            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripButtonAgregar,
-            this.toolStripButtonEditar,
-            this.toolStripButtonEliminar});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(535, 25);
-            this.toolStrip1.TabIndex = 10;
-            this.toolStrip1.Text = "toolStrip1";
-            // 
-            // toolStripButtonAgregar
-            // 
-            this.toolStripButtonAgregar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButtonAgregar.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonAgregar.Image")));
-            this.toolStripButtonAgregar.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButtonAgregar.Name = "toolStripButtonAgregar";
-            this.toolStripButtonAgregar.Size = new System.Drawing.Size(23, 22);
-            this.toolStripButtonAgregar.Text = "Agregar";
-            this.toolStripButtonAgregar.CheckStateChanged += new System.EventHandler(this.toolStripButtonAgregar_CheckStateChanged);
-            // 
-            // toolStripButtonEditar
-            // 
-            this.toolStripButtonEditar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButtonEditar.Image = global::BrainW.Properties.Resources.user_32;
-            this.toolStripButtonEditar.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButtonEditar.Name = "toolStripButtonEditar";
-            this.toolStripButtonEditar.Size = new System.Drawing.Size(23, 22);
-            this.toolStripButtonEditar.Text = "Editar";
-            // 
-            // toolStripButtonEliminar
-            // 
-            this.toolStripButtonEliminar.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
-            this.toolStripButtonEliminar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButtonEliminar.Image = global::BrainW.Properties.Resources.user_22;
-            this.toolStripButtonEliminar.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButtonEliminar.Name = "toolStripButtonEliminar";
-            this.toolStripButtonEliminar.Size = new System.Drawing.Size(23, 22);
-            this.toolStripButtonEliminar.Text = "Eliminar";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(323, 89);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(22, 20);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 10;
-            this.pictureBox1.TabStop = false;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.AllowUserToResizeColumns = false;
-            this.dataGridView1.AllowUserToResizeRows = false;
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 236);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
-            this.dataGridView1.Size = new System.Drawing.Size(511, 150);
-            this.dataGridView1.TabIndex = 11;
-            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
-            // 
-            // pfpa92DataSet
-            // 
-            this.pfpa92DataSet.DataSetName = "pfpa92DataSet";
-            this.pfpa92DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // tblusuariosBindingSource
-            // 
-            this.tblusuariosBindingSource.DataMember = "tblusuarios";
-            this.tblusuariosBindingSource.DataSource = this.pfpa92DataSet;
-            // 
-            // tblusuariosTableAdapter
-            // 
-            this.tblusuariosTableAdapter.ClearBeforeFill = true;
             // 
             // txtID
             // 
             this.txtID.Enabled = false;
             this.txtID.Location = new System.Drawing.Point(196, 11);
             this.txtID.Name = "txtID";
-            this.txtID.Size = new System.Drawing.Size(121, 20);
+            this.txtID.Size = new System.Drawing.Size(281, 20);
             this.txtID.TabIndex = 12;
             // 
-            // lblID
+            // pictureBox1
             // 
-            lblID.AutoSize = true;
-            lblID.Location = new System.Drawing.Point(57, 18);
-            lblID.Name = "lblID";
-            lblID.Size = new System.Drawing.Size(18, 13);
-            lblID.TabIndex = 13;
-            lblID.Text = "ID";
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(483, 89);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(22, 20);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.AutoSize = false;
+            this.toolStrip1.CanOverflow = false;
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButtonAgregar,
+            this.toolStripButtonEditar,
+            this.toolStripButtonEliminar});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 374);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.toolStrip1.Size = new System.Drawing.Size(535, 41);
+            this.toolStrip1.TabIndex = 10;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButtonAgregar
+            // 
+            this.toolStripButtonAgregar.AutoSize = false;
+            this.toolStripButtonAgregar.BackgroundImage = global::BrainW.Properties.Resources.user_20;
+            this.toolStripButtonAgregar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.toolStripButtonAgregar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.None;
+            this.toolStripButtonAgregar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonAgregar.Name = "toolStripButtonAgregar";
+            this.toolStripButtonAgregar.Size = new System.Drawing.Size(40, 40);
+            this.toolStripButtonAgregar.Text = "Agregar";
+            this.toolStripButtonAgregar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.toolStripButtonAgregar.CheckStateChanged += new System.EventHandler(this.toolStripButtonAgregar_CheckStateChanged);
+            this.toolStripButtonAgregar.Click += new System.EventHandler(this.toolStripButtonAgregar_Click);
+            // 
+            // toolStripButtonEditar
+            // 
+            this.toolStripButtonEditar.AutoSize = false;
+            this.toolStripButtonEditar.BackgroundImage = global::BrainW.Properties.Resources.user_32;
+            this.toolStripButtonEditar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.toolStripButtonEditar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.None;
+            this.toolStripButtonEditar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonEditar.Name = "toolStripButtonEditar";
+            this.toolStripButtonEditar.Size = new System.Drawing.Size(40, 40);
+            this.toolStripButtonEditar.Text = "Editar";
+            this.toolStripButtonEditar.Click += new System.EventHandler(this.toolStripButtonEditar_Click);
+            // 
+            // toolStripButtonEliminar
+            // 
+            this.toolStripButtonEliminar.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripButtonEliminar.AutoSize = false;
+            this.toolStripButtonEliminar.BackgroundImage = global::BrainW.Properties.Resources.user_22;
+            this.toolStripButtonEliminar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.toolStripButtonEliminar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.None;
+            this.toolStripButtonEliminar.Image = global::BrainW.Properties.Resources.user_22;
+            this.toolStripButtonEliminar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonEliminar.Name = "toolStripButtonEliminar";
+            this.toolStripButtonEliminar.Size = new System.Drawing.Size(40, 40);
+            this.toolStripButtonEliminar.Text = "Eliminar";
+            this.toolStripButtonEliminar.Click += new System.EventHandler(this.toolStripButtonEliminar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnF2;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 228);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(535, 146);
+            this.dataGridView1.TabIndex = 11;
+            this.dataGridView1.CellStateChanged += new System.Windows.Forms.DataGridViewCellStateChangedEventHandler(this.dataGridView1_CellStateChanged);
+            // 
+            // tblusuariosBindingSource
+            // 
+            this.tblusuariosBindingSource.DataMember = "tblusuarios";
+            this.tblusuariosBindingSource.DataSource = this.pfpa92DataSet;
+            // 
+            // pfpa92DataSet
+            // 
+            this.pfpa92DataSet.DataSetName = "pfpa92DataSet";
+            this.pfpa92DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tblusuariosTableAdapter
+            // 
+            this.tblusuariosTableAdapter.ClearBeforeFill = true;
             // 
             // frmUsuarios
             // 
+            this.AcceptButton = this.bttnAceptar;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(535, 415);
             this.Controls.Add(this.dataGridView1);
@@ -275,14 +298,13 @@
             this.Load += new System.EventHandler(this.frmUsuarios_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pfpa92DataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.tblusuariosBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pfpa92DataSet)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
