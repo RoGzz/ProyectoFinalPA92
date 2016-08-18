98a72e06b8046531b58ee4cab2b0af2abc07ce39
Roberto González Cruz <the-king_bone@hotmail.com>
2016-08-17 18:34:23 -0700

    cambios

--- BrainW/BrainW/Program.cs
+++ BrainW/BrainW/Program.cs
@@ -16,7 +16,7 @@ namespace BrainW
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMenu());
+            Application.Run(new frmInicioSesion());
         }
     }
 }
